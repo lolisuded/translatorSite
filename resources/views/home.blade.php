0d7a6c5526443d5b7c30547aea4dbfcbbf2ad844
yohanne <lolisuded12@gmail.com>
2020-05-02 17:28:14 +0200

    Added Vertalen Text. --- Added more sizing according to screen.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,18 @@
 <div class="brandSlogan" data-aos="fade-right" data-aos-delay="400" data-aos-duration="3000">
     <h3>let's Talk.</h3>
 </div>
+<div class="TolkVertaal-Container">
+    <div class="TolkenText">
+        <h1>Tolk</h1>
+        <div class="TOLKsplitline"></div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officia eveniet dolor commodi ex eum? Suscipit sit illum dolorem? Magnam distinctio deserunt ea. Reiciendis fuga ex est officia, odit ea.</p>
+    </div>
 
-<div class="TolkenText">
-    <h1>Tolk</h1>
-    <div class="TOLKsplitline"></div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officia eveniet dolor commodi ex eum? Suscipit sit illum dolorem? Magnam distinctio deserunt ea. Reiciendis fuga ex est officia, odit ea.</p>
+    <div class="VertalenText">
+        <h1>Vertalen</h1>
+        <div class="VERTAALsplitline"></div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt veritatis ea reprehenderit veniam, minima aut porro id modi nam! Enim dicta veritatis odio assumenda illo recusandae expedita tenetur quisquam architecto.</p>
+    </div>
 </div>
 @endsection
 
