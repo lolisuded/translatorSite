9a82fde04dec1f9da947e33a43896117880143f7
Anher 47 <anhur.mufq@gmail.com>
2020-05-04 01:15:43 +0200

    Updated style of Offer's view.

--- resources/views/offer.blade.php
+++ resources/views/offer.blade.php
@@ -1,15 +1,55 @@
 @extends('base.base')
-@section('body') 
+@section('body')
 
 <h1>Offerte</h1>
 <form method="POST" action="/offer/post">
     @csrf
-    <input type="text" placeholder="Voornaam" name="firstname" required>
-    <input type="text" placeholder="Achternaam" name="lastname" required>
-    <input type="email" placeholder="E-mail" name="email" required>
-    <input type="text" placeholder="Telefoonummer" name="telnumber" required>
-    <textarea placeholder="Uw bericht!" name="msg" required></textarea>
-    <button type="submit" class="btn btn-primary text-white">Verstuur</a>
-    
+    <div class="ml-2 form-row">
+        <div class="col-md-3 mb-3">
+            <label class="sr-only" for="firstname">Voornaam</label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">Voornaam</div>
+                </div>
+                <input type="text" class="form-control" id="firstname" placeholder="" name="firstname" required>
+            </div>
+        </div>
+
+        <div class="col-md-3 mb-3">
+            <label class="sr-only" for="lastname">Achternaam</label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">Achternaam</div>
+                </div>
+                <input type="text" class="form-control" id="lastname" placeholder="" name="lastname" required>
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group col-md-4">
+        <label class="sr-only" for="email">E-mail</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <div class="input-group-text">E-mail</div>
+            </div>
+            <input type="email" class="form-control" id="email" placeholder="" name="email" required>
+        </div>
+    </div>
+
+    <div class="form-group col-md-4">
+        <div class="input-group">
+            <label class="sr-only" for="telnumber">Telefoonummer</label>
+            <div class="input-group-prepend">
+                <div class="input-group-text">Telefoonummer</div>
+            </div>
+            <input type="text" class="form-control" id="telnumber" placeholder="" name="telnumber" required>
+        </div>
+    </div>
+
+    <div class="mt-5 form-group col-md-4">
+        <label class="text-white" for="msg">Uw bericht!</label>
+        <textarea class="form-control form-control-lg" id="msg" placeholder="" name="msg" required></textarea>
+    </div>
+    <button type="submit" class="ml-3 btn btn-primary text-white">Verstuur</button>
 </form>
 @endsection
