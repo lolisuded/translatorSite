1b843264f4311dce2e4a22e726d512f6fc2e93c3
Anher 47 <anhur.mufq@gmail.com>
2020-05-05 01:04:57 +0200

    Created DB migrations.

---
+++ database/migrations/2020_05_04_221323_create_offers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->foreignId('service_id')->references('id')->on('services');
+            $table->foreignId('language_id')->references('id')->on('languages');;
+            $table->string('email');
+            $table->string('telnumber');
+            $table->string('message');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
