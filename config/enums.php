7d655ec2d072a6747d7f2e2ff3431f3051492dbe
Anher 47 <anhur.mufq@gmail.com>
2020-05-05 01:05:31 +0200

    Created enums file.

---
+++ config/enums.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'services' => [
+        'Translate' => '1',
+        'Interpret' => '2'
+    ],
+
+    'languages' => [
+        'DutchToEnglish' => '1',
+        'EnglishToDutch' => '2',
+        'DutchToArabic' => '3',
+        'ArabicToDutch' => '4'
+    ]
+];
