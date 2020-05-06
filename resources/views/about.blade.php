8710fe2af8c1a48b415480b657767696e1cefa02
yohanne <lolisuded12@gmail.com>
2020-05-06 23:41:02 +0200

    Added basic About me --- Added Font

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,16 @@
 @extends('base.base')
 @section('body') 
 
-<h1>Over mij</h1>
+<div class="About-container">
+    <div class="AboutText">
+        <h1 class="AboutTitle">Over mij</h1>
+        <div class="Aboutsplitline"></div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quibusdam labore, deserunt optio debitis, rem ab consequuntur aperiam, exercitationem numquam reprehenderit asperiores distinctio. Culpa in sapiente quis repellat nemo consequuntur.</p>
+    </div>
+    <div class="AnwarPhoto">
+        <img src="images/anwar.jpg">
+    </div>
+</div>
+
 
 @endsection
