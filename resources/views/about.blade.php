3498175e6cc197b185c8d9e1c68269749e20096a
yohanne <lolisuded12@gmail.com>
2020-05-07 02:22:42 +0200

    Added an overlay when you Hover over photo.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,10 +7,18 @@
         <div class="Aboutsplitline"></div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quibusdam labore, deserunt optio debitis, rem ab consequuntur aperiam, exercitationem numquam reprehenderit asperiores distinctio. Culpa in sapiente quis repellat nemo consequuntur.</p>
     </div>
+
+    
     <div class="AnwarPhoto">
         <img src="images/anwar.jpg">
+        <div class="AnwarPhoto-Items">
+            <h2>Bekijk Me Op Linked-In!</h2>
+            <p>
+                <a href="https://nl.linkedin.com/in/anwar-al-sabiry-95119728" target="_blank"><i class="fab fa-linkedin"></i></a>
+            </p>
+        </div>
     </div>
-</div>
+
 
 
 @endsection
